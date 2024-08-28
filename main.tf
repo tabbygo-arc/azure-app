011227ebdd9a54aeb2a5d1e164c340ca30bab49a
tabbygo-arc <118011987+tabbygo-arc@users.noreply.github.com>
2024-08-28 17:13:08 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ data "azurerm_resource_group" "tfexample" {
 # Create a Virtual Network
 resource "azurerm_virtual_network" "tfexample" {
   name                = "my-terraform-vnet"
-  location            = dayta.azurerm_resource_group.tfexample.location
+  location            = data.azurerm_resource_group.tfexample.location
   resource_group_name = data.azurerm_resource_group.tfexample.name
   address_space       = ["10.0.0.0/16"]
 
